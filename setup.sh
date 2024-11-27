dc51755dd2ad8cde16918373f671d794f31d4bc7
DavidUdell <udelldavidb@gmail.com>
2024-11-27 23:06:14 +0000

    Feature: add setup.sh for environment setup

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Setup pipeline script
+pip install -r requirements.txt
+git submodule update --init
+pip install -r dictionary_learning/requirements.txt
+
+pip uninstall torchvision
+pip install -U torch
+
+python3 circuit.py > j_log.txt
